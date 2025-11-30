19e36e8c5363d2b1a24e836ae54b3566029bdd22
Sinval Luz <contato.sinvalluz@gmail.com>
2025-11-30 15:54:35 -0300

    feat: add product service

---
+++ src/main/java/com/sinvaldev/gerenciadordeestoque/service/ProductService.java
@@ -0,0 +1,56 @@
+package com.sinvaldev.gerenciadordeestoque.service;
+
+
+import com.sinvaldev.gerenciadordeestoque.entity.Product;
+import com.sinvaldev.gerenciadordeestoque.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void createProduct (Product product) {
+        productRepository.save(product);
+    }
+
+    public List<Product> findAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public List<Product> findByCategory(Integer id) {
+        List<Product> products = productRepository.findByCategoryId(id);
+
+        if (products.isEmpty()) {
+            throw new RuntimeException("Nenhum produto encontrado para a categoria ID: " + id);
+        }
+
+        return products;
+    }
+
+    public Product updateProduct(Integer id, Product product) {
+        Product productEntity = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Nenhum produto encontrado para a categoria ID: " + id));
+
+        Product productUpdated = Product.builder()
+                .id(productEntity.getId())
+                .name(product.getName() != null ? product.getName() : productEntity.getName())
+                .quantity(product.getQuantity() != null ? product.getQuantity() : productEntity.getQuantity()).build();
+
+        return productRepository.save(productUpdated);
+    }
+
+    public void delete(Integer id) {
+        productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Nenhum produto encontrado com o id: " + id));
+
+        productRepository.deleteById(id);
+    }
+
+}
